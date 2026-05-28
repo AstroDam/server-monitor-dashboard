8b23d571e350644fd1b6a5280de0a03c624d19e3
AstroDam <damalvesmeira48@gmail.com>
2026-05-28 02:51:43 -0300

    Add incident SLA engine

--- agent/agent.js
+++ agent/agent.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const crypto = require('crypto');
 
-const pool = require('../db');
 const authMiddleware = require('../middlewares/auth');
 const createLog = require('../utils/logger');
 
